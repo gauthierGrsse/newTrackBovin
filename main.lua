1f899323c9e501a130fe3c1b2b5a2e8e6f5d7a55
Gauthier G <gguerisse@icloud.com>
2022-08-23 13:45:26 -0600

    v1 generation macro, timecode

--- main.lua
+++ main.lua
@@ -1,8 +1,14 @@
 -- Create new track
 -- Bovin show de base
 -- Par Gauthier Guerisse
-
 -- Variable :
+local speedMaster = 16
+local mainExecId = 1
+
+local startPageExec = 0
+local startMacro = 76
+local startTimecode = 0
+local startView = 2000
 
 -- Setup :
 local haveFeedback = true
@@ -36,8 +42,38 @@ local function error(text)
     feedback("Trackshow plugin ERROR : " .. text)
 end
 
-local function start(arg)
+local function createMacroLine(macro, line, command)
+    cmd('Store Macro 1.' .. macro .. '.' .. line .. ' "' .. command .. '"')
+end
 
+local function createMacro(id, name)
+    local macroId = startMacro + id
+    cmd('Store Macro ' .. macroId .. ' "' .. name .. '"')
+    createMacroLine(macroId, 1, "Page " .. startPageExec + id)
+    createMacroLine(macroId, 2, 'Select Executor 1')
+    createMacroLine(macroId, 3, 'Fader 1 At Full')
+    createMacroLine(macroId, 4, 'Appearance Macro 77 Thru 106 /h=0 /s=100 /br=100')
+    createMacroLine(macroId, 5, 'Appearance Macro ' .. macroId .. ' /h=120 /s=100 /br=100')
+    createMacroLine(macroId, 6, 'Copy View 289 At View 301 /o')
+    createMacroLine(macroId, 7, 'Kill 1')
+    createMacroLine(macroId, 8, 'View 301')
+    createMacroLine(macroId, 9, 'Macro 125')
+    createMacroLine(macroId, 10, 'Fader 1 At Full')
+    createMacroLine(macroId, 11, 'Go Executor 1 Cue 1')
+    createMacroLine(macroId, 12, 'SpecialMaster 3.' .. speedMaster .. ' At ' .. (textinput('BPM Track ?', '120')))
+    createMacroLine(macroId, 13, 'Off Timecode 1 Thru')
+    createMacroLine(macroId, 14, 'Go Timecode ' .. startTimecode + id)
+end
+
+local function start(arg)
+    local trackId = tonumber(textinput('ID nouveau track', '0'))
+    local trackName = (textinput('Nom nouveau track ?', 'Ma bite'))
+    if confirm('Confirmer la création', 'Confirmer la création d\'une nouvelle track avec l\'ID ' .. trackId) then
+        createMacro(trackId, trackName)
+        cmd('Store Timecode ' .. startTimecode + trackId .. ' "' .. trackName .. '"')
+    else
+        msgbox('Création annulée', 'Création du track (' .. trackName .. ', ID ' .. trackId .. ') annulé')
+    end
 end
 
 return start
